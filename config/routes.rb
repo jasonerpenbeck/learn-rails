5a4186ae776722946a63effe403e81c89b795461
Jason Erpenbeck <jason.erpenbeck@gmail.com>
2013-09-08 14:40:07 -0700

    About and Contact Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 LearnRails::Application.routes.draw do
   root to: 'visitors#new'
+
 end
+
+
+#  get "/pages/*id", to: 'pages#show'
+#pages controller is replaced by High Voltage gem that routes things to app/views/pages.  See line above for what it would look like if defined in routes.rb
